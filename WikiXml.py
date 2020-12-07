cb9ee87e16f8b9b4fa32375f9ab909a4c98b9eb0
Shinjini Ghosh <shinghos@mit.edu>
2020-12-06 20:19:51 -0500

    more parsing attempts

---
+++ WikiXml.py
@@ -0,0 +1,31 @@
+import xml.sax
+
+
+class WikiXmlHandler(xml.sax.handler.ContentHandler):
+    """Content handler for Wiki XML data using SAX"""
+
+    def __init__(self):
+        xml.sax.handler.ContentHandler.__init__(self)
+        self._buffer = None
+        self._values = {}
+        self._current_tag = None
+        self._pages = []
+
+    def characters(self, content):
+        """Characters between opening and closing tags"""
+        if self._current_tag:
+            self._buffer.append(content)
+
+    def startElement(self, name, attrs):
+        """Opening tag of element"""
+        if name in ('title', 'text', 'timestamp'):
+            self._current_tag = name
+            self._buffer = []
+
+    def endElement(self, name):
+        """Closing tag of element"""
+        if name == self._current_tag:
+            self._values[name] = ' '.join(self._buffer)
+
+        if name == 'page':
+            self._pages.append((self._values['title'], self._values['text']))
--- wikipedia_fr_parse.py
+++ wikipedia_fr_parse.py
@@ -5,7 +5,14 @@ from timeit import default_timer as timer
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+import bz2
+import subprocess
+import xml.sax
+from WikiXml import WikiXmlHandler
+import wiki_dump_parser as parser
 
+
+# get wikipedia files
 base_url = 'https://dumps.wikimedia.org/frwiki/'
 index = requests.get(base_url).text
 soup_index = BeautifulSoup(index, 'html.parser')
@@ -46,6 +53,7 @@ for file in files_to_download:
         file_number = int(file.split('p')[-1].split('.')[-2]) - int(file.split('p')[-2])
         file_info.append((file.split('-')[-1], file_size, file_number))
 
+# inspect file sizes
 print("The largest files are:")
 print("Filename, file size, number of articles")
 for thing in sorted(file_info, key=lambda x: x[1], reverse=True)[:5]:
@@ -55,6 +63,37 @@ print(f'There are {len(file_info)} partitions.')
 
 file_df = pd.DataFrame(file_info, columns = ['file', 'size (MB)', 'articles']).set_index('file')
 file_df['size (MB)'].plot.bar(color = 'red', figsize = (12, 6))
-plt.show()
+# plt.show()
 
 print(f"The total size of files on disk is {file_df['size (MB)'].sum() / 1e3} GB")
+
+# print(data_paths)
+data_path = data_paths[-1]
+
+# inspect data
+# lines = []
+#
+# for i, line in enumerate(subprocess.Popen(['bzcat'],
+#                                           stdin=open(data_path),
+#                                           stdout=subprocess.PIPE).stdout):
+#     lines.append(line)
+#     if i > 5e5:
+#         break
+
+# print(lines[-165:-109])
+
+
+# Content handler for Wiki XML
+# handler = WikiXmlHandler()
+
+# Parsing object
+# parser = xml.sax.make_parser()
+# parser.setContentHandler(handler)
+# for l in lines[-165:-109]:
+#     parser.feed(l)
+#
+# print(handler._pages)
+
+# ALTERNATIVE
+xml_path = data_path[:-4]
+print("XML to use is", xml_path)
